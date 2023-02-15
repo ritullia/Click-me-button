fe08c2dc5ff61d889b521d616f95d5a42ebe7979
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-15 19:53:53 +0200

    Dev (#1)
    * element & addEvList button commit
    
    * count function & counting commit

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+console.log("veikia")
+
+
+const clickBtn = document.getElementById('btn__element');
+
+let count = 0
+
+function countClicks() {
+
+    count++;
+
+    let countClick = document.getElementById('btn__state');
+
+    countClick.innerHTML = count;
+}
+
+clickBtn.addEventListener('click', countClicks)
